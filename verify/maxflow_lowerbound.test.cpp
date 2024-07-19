27aa2acb11a87365ffa59cb80a07986aad7d3520
Astral-23 <ul7cr38@gmail.com>
2024-07-19 20:47:05 +0900

    e

--- verify/maxflow_lowerbound.test.cpp
+++ verify/maxflow_lowerbound.test.cpp
@@ -10,6 +10,7 @@ int main() {
     int n, m;
     while (1) {
         cin >> n >> m;
+
         if (n == 0) break;
 
         vector<pll> es;
@@ -20,38 +21,44 @@ int main() {
             es.emplace_back(u, v);
         }
 
-        int w = n + 100;
         int ans_m = -1;
         int ans_M = -1;
 
+        auto che = [&](int mi, int Mi) {
+            int s = n + m;
+            int t = s + 1;
 
+            mf_graph_with_lowerbound mf(t + 1);
 
+            rep(i, 0, m) {
+                mf.add_edge(s, i, 1, 1);
+                mf.add_edge(i, m + es[i].first, 0, 1);
+                mf.add_edge(i, m + es[i].second, 0, 1);
+            }
 
-        rep(wi, 0, m + 1) {
-            rrep(mi, 0, m + 1) {
-                int Mi = mi + wi;
-                if(Mi > m) continue;
-                int s = n + m;
-                int t = s + 1;
+            rep(i, 0, n) { mf.add_edge(m + i, t, mi, Mi); }
 
-                mf_graph_with_lowerbound mf(t + 1);
+            if (mf.is_feasable(s, t)) {
+                return true;
+            } else
+                return false;
+        };
 
-                rep(i, 0, m) {
-                    mf.add_edge(s, i, 1, 1);
-                    mf.add_edge(i, m + es[i].first, 0, 1);
-                    mf.add_edge(i, m + es[i].second, 0, 1);
+        ll w = 1e7;
+        rrep(mi, 0, n + 1) {
+            ll li = 0;
+            ll ri = li + min<ll>(w, m);
+            while (li < ri) {  /// xxxxoooo
+                ll mid = (li + ri) / 2;
+                if (che(mi, mi + mid)) {
+                    ri = mid;
+                } else {
+                    li = mid + 1;
                 }
-
-                rep(i, 0, n) { mf.add_edge(m + i, t, mi, Mi); }
-
-                if (mf.is_feasable(s, t)) {
-                    if (chmin(w, Mi - mi)) {
-                        ans_m = mi;
-                        ans_M = Mi;
-                        wi = m;
-                        mi = m;
-                    }
-                }
+            }
+            if (che(mi, mi + li) && chmin(w, li - mi)) {
+                ans_m = mi;
+                ans_M = mi + li;
             }
         }
 
