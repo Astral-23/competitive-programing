f7fe6be208338378aa914ecf00a9c1c064d209ac
Astral-23 <ul7cr38@gmail.com>
2024-07-31 11:16:26 +0900

    やっぱこうで

--- verify/superset_transform.test.cpp
+++ verify/superset_transform.test.cpp
@@ -13,6 +13,25 @@ S inv(S x) {
     return -1 * x;
 }
 
+S zero() {
+    return 0;
+}
+
+template<class S, S (*op)(S, S), S (*inv)(S),  S(*zero)()> vec<S> bitwise_and_convolution(vec<S> A, vec<S> B) {
+    ll lg = 1;
+    while(A.size() > (1LL << lg)) lg++;
+    while(B.size() > (1LL << lg)) lg++;
+    A.resize(1LL << lg, zero());
+    B.resize(1LL << lg, zero());
+
+    vec<mint> FA = superset_zeta_transform<S, op>(A, lg);
+    vec<mint> FB = superset_zeta_transform<S, op>(B, lg);
+    rep(i, 0, 1 << lg) FA[i] *= FB[i];
+    vec<mint> f = superset_mobius_transform<S, op, inv>(FA, lg);
+    return f;
+}
+//以降の項について、0である。
+
 int main() {
     int n;
     cin >> n;
@@ -20,13 +39,7 @@ int main() {
     rep(i, 0, 1 << n) cin >> A[i];
     rep(i, 0, 1 << n) cin >> B[i];
 
-    vec<mint> FA = superset_zeta_transform<S, op>(A, n);
-    vec<mint> FB = superset_zeta_transform<S, op>(B, n);
-
-    rep(i, 0, 1 << n) FA[i] *= FB[i];
-
-    vec<mint> f = superset_mobius_transform<S, op, inv>(FA, n);
-
+    auto f = bitwise_and_convolution<S, op, inv, zero>(A, B);
     rep(i, 0, 1 << n) cout << f[i] << " ";
     cout << endl;
 
