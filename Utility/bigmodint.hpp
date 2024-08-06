dbd7b71dd72e4d52366499ffbadffe2c616f0c87
Astral-23 <ul7cr38@gmail.com>
2024-08-06 18:23:20 +0900

    aaa

--- Utility/bigmodint.hpp
+++ Utility/bigmodint.hpp
@@ -11,25 +11,24 @@ uint32_t x → uint64_t x
 (uint64_t)(a.x) * b.x; → (__uint128_t)(a.x) * b.x;
 */
 
-//動的mod : template<int mod> を消して、上の方で変数modを宣言
-template<uint64_t mod>
-struct modint{
+// 動的mod : template<int mod> を消して、上の方で変数modを宣言
+template <uint64_t mod> struct modint {
     using mm = modint;
     uint64_t x;
 
-    modint() : x(0){}
-    TT modint(T a=0) : x((__int128_t(a) % mod + mod)){
-        if(x >= mod) x -= mod;
+    modint() : x(0) {}
+    TT modint(T a = 0) : x((__int128_t(a) % mod + mod)) {
+        if (x >= mod) x -= mod;
     }
 
     friend mm operator+(mm a, mm b) {
         a.x += b.x;
-        if(a.x >= mod) a.x -= mod;
+        if (a.x >= mod) a.x -= mod;
         return a;
     }
-   friend mm operator-(mm a, mm b) {
+    friend mm operator-(mm a, mm b) {
         a.x -= b.x;
-        if(a.x >= mod) a.x += mod;
+        if (a.x >= mod) a.x += mod;
         return a;
     }
 
@@ -37,36 +36,37 @@ struct modint{
 
     friend mm operator*(mm a, mm b) { return (__uint128_t)(a.x) * b.x; }
     friend mm operator/(mm a, mm b) { return a * b.inv(); }
-    friend mm& operator+=(mm& a, mm b) { return a = a + b; }
-    friend mm& operator-=(mm& a, mm b) { return a = a - b; }
-    friend mm& operator*=(mm& a, mm b) { return a = a * b; }
-    friend mm& operator/=(mm& a, mm b) { return a = a * b.inv(); }
+    friend mm &operator+=(mm &a, mm b) { return a = a + b; }
+    friend mm &operator-=(mm &a, mm b) { return a = a - b; }
+    friend mm &operator*=(mm &a, mm b) { return a = a * b; }
+    friend mm &operator/=(mm &a, mm b) { return a = a * b.inv(); }
 
-    //ロリハならここまで(/演算と /= 演算はいらない)
+    // ロリハならここまで(/演算と /= 演算はいらない)
 
-    mm inv() const {return pow(mod-2);}
-    mm pow(const ll& y) const {
-        if(!y) return 1;
-        mm res = pow(y >> 1);
-        res *= res;
-        if(y & 1) res *= *this;
+    mm inv() const { return pow(mod - 2); }
+    mm pow(ll y) const {
+        mm res = 1;
+        mm v = *this;
+        while (y) {
+            if (y & 1) res *= v;
+            v *= v;
+            y /= 2;
+        }
         return res;
     }
 
-    friend istream& operator>>(istream &is, mm &a) { 
+    friend istream &operator>>(istream &is, mm &a) {
         ll t;
         cin >> t;
         a = mm(t);
         return is;
     }
 
-    friend ostream& operator<<(ostream &os, mm a) {
-        return os << a.x;
-    }
+    friend ostream &operator<<(ostream &os, mm a) { return os << a.x; }
 
-    bool operator==(mm a) {return x == a.x;}
-    bool operator!=(mm a) {return x != a.x;}
-    //bool operator<(const mm& a) const {return x < a.x;}
+    bool operator==(mm a) { return x == a.x; }
+    bool operator!=(mm a) { return x != a.x; }
+    // bool operator<(const mm& a) const {return x < a.x;}
 };
 
 using rhash = modint<(1LL << 61) - 1>;
--- Utility/modint.hpp
+++ Utility/modint.hpp
@@ -1,22 +1,21 @@
 
-//動的mod : template<int mod> を消して、上の方で変数modを宣言
-template<uint32_t mod>
-struct modint{
+// 動的mod : template<int mod> を消して、上の方で変数modを宣言
+template <uint32_t mod> struct modint {
     using mm = modint;
     uint32_t x;
     modint() : x(0) {}
-    TT modint(T a=0) : x((ll(a) % mod + mod)){
-        if(x >= mod) x -= mod;
+    TT modint(T a = 0) : x((ll(a) % mod + mod)) {
+        if (x >= mod) x -= mod;
     }
 
     friend mm operator+(mm a, mm b) {
         a.x += b.x;
-        if(a.x >= mod) a.x -= mod;
+        if (a.x >= mod) a.x -= mod;
         return a;
     }
-   friend mm operator-(mm a, mm b) {
+    friend mm operator-(mm a, mm b) {
         a.x -= b.x;
-        if(a.x >= mod) a.x += mod;
+        if (a.x >= mod) a.x += mod;
         return a;
     }
 
@@ -24,35 +23,36 @@ struct modint{
 
     friend mm operator*(mm a, mm b) { return (uint64_t)(a.x) * b.x; }
     friend mm operator/(mm a, mm b) { return a * b.inv(); }
-    friend mm& operator+=(mm& a, mm b) { return a = a + b; }
-    friend mm& operator-=(mm& a, mm b) { return a = a - b; }
-    friend mm& operator*=(mm& a, mm b) { return a = a * b; }
-    friend mm& operator/=(mm& a, mm b) { return a = a * b.inv(); }
+    friend mm &operator+=(mm &a, mm b) { return a = a + b; }
+    friend mm &operator-=(mm &a, mm b) { return a = a - b; }
+    friend mm &operator*=(mm &a, mm b) { return a = a * b; }
+    friend mm &operator/=(mm &a, mm b) { return a = a * b.inv(); }
 
-    mm inv() const {return pow(mod-2);}
-    mm pow(const ll& y) const {
-        if(!y) return 1;
-        mm res = pow(y >> 1);
-        res *= res;
-        if(y & 1) res *= *this;
+    mm inv() const { return pow(mod - 2); }
+    mm pow(ll y) const {
+        mm res = 1;
+        mm v = *this;
+        while (y) {
+            if (y & 1) res *= v;
+            v *= v;
+            y /= 2;
+        }
         return res;
     }
 
-    friend istream& operator>>(istream &is, mm &a) { 
+    friend istream &operator>>(istream &is, mm &a) {
         ll t;
         cin >> t;
         a = mm(t);
         return is;
     }
 
-    friend ostream& operator<<(ostream &os,  mm a) {
-        return os << a.x;
-    }
+    friend ostream &operator<<(ostream &os, mm a) { return os << a.x; }
 
-    bool operator==(mm a) {return x == a.x;}
-    bool operator!=(mm a) {return x != a.x;}
+    bool operator==(mm a) { return x == a.x; }
+    bool operator!=(mm a) { return x != a.x; }
 
-    //bool operator<(const mm& a) const {return x < a.x;}
+    // bool operator<(const mm& a) const {return x < a.x;}
 };
 
 using modint998244353 = modint<998244353>;
