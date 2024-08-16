8bb449ce67de0a41e3ee0396876f2052f2aed9b0
Astral-23 <ul7cr38@gmail.com>
2024-08-16 18:11:07 +0900

    bisect

---
+++ Algorithm/bisect.hpp
@@ -0,0 +1,15 @@
+template <typename T, typename F> T bisect(T ok, T ng, F pred) {
+    if(ok <= ng) ng++;
+    else ng--;
+    
+    if (!pred(ok)) return ok;
+
+    while (ng > ok + 1 || ok > ng + 1) {
+        T mid = midpoint(ok, ng);
+        (pred(mid) ? ok : ng) = mid;
+    }
+    return ok;
+}
+/*
+@brief 抽象化二分探索
+*/
---
+++ verify/bisect.test.cpp
@@ -0,0 +1,31 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/enumerate_quotients"
+#include "../Utility/template.hpp"
+#include "../Algorithm/bisect.hpp"
+
+/*
+n/x - n/(x+1) >= 1
+⇔
+n >= x(x+1)
+
+*/
+int main() {
+    ll n;
+    cin >> n;
+    
+    auto ok = [&](ll x) {
+        return n >= __int128_t(x)*(x+1);
+    };
+
+    ll x = bisect(0LL, n, ok);
+
+    vec<ll> ans;
+    rep(i, 1, x+1) ans.push_back(n / i);
+   
+    for(ll i = n/(x+1); i >= 1; i--) ans.push_back(i);
+
+    reverse(all(ans));
+    cout << ans.size() << endl;
+    rep(i, 0, ans.size()) cout << ans[i] <<" ";
+
+
+}
