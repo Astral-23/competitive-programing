14ae4cf98d9679ee979db317a0e1ac25da98b7dc
Astral-23 <ul7cr38@gmail.com>
2024-08-16 18:30:37 +0900

    inde

--- Algorithm/bisect.hpp
+++ Algorithm/bisect.hpp
@@ -1,9 +1,10 @@
 template <typename T, typename F> T bisect(T ok, T ng, F pred) {
-    if(ok <= ng) ng++;
-    else ng--;
-    
-    if (!pred(ok)) return ok;
+    if (ok <= ng)
+        ng++;
+    else
+        ng--;
 
+    if (!pred(ok)) return ok;
 
     while (ng > ok + 1 || ok > ng + 1) {
         T mid = ((ok ^ ng) >> 1) + (ok & ng);
