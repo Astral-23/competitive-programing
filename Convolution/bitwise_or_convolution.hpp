eaad3dfbb291ea3117f08c988b89584838288249
Astral-23 <ul7cr38@gmail.com>
2024-08-18 17:09:25 +0900

    a

---
+++ Convolution/bitwise_or_convolution.hpp
@@ -0,0 +1,16 @@
+#include "../Algorithm/subset_transform.hpp"
+
+template<class S, S (*op)(S, S), S (*inv)(S),  S(*zero)()> vec<S> bitwise_or_convolution(vec<S> A, vec<S> B) {
+    ll lg = 1;
+    while(A.size() > (1LL << lg)) lg++;
+    while(B.size() > (1LL << lg)) lg++;
+    A.resize(1LL << lg, zero());
+    B.resize(1LL << lg, zero());
+
+    vec<S> FA = subset_zeta_transform<S, op>(A, lg);
+    vec<S> FB = subset_zeta_transform<S, op>(B, lg);
+    rep(i, 0, 1 << lg) FA[i] *= FB[i];
+    vec<S> f = subset_mobius_transform<S, op, inv>(FA, lg);
+    return f;
+}
+//以降の項について、0である。
---
+++ verify/Chromatic_Number_with_restoration.test.cpp.cpp
@@ -0,0 +1,32 @@
+#include "../Utility/template.hpp"
+#include "../Gragh/Chromatic_Number_with_restoration.hpp"
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vec<vec<int>> g(n);
+    rep(i, 0, m) {
+        int u, v;
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+    auto [ans, col] = chromatic_number(g);
+
+    vec<int> cnt(ans, 0);
+
+    rep(i, 0, n) {
+        assert(0 <= col[i]);
+        assert(col[i] < ans);
+        cnt[col[i]]++;
+        for(int to : g[i]) {
+            assert(col[i] != col[to]);
+        }
+    }
+
+    rep(i, 0, ans) assert(cnt[i] != 0);
+
+    cout << ans << endl;
+
+}
