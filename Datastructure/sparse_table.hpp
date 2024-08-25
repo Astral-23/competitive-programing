ce46899b2fbf9e6752142e9143ae2810620e73d8
Astral-23 <ul7cr38@gmail.com>
2024-08-26 00:04:03 +0900

    sparse_tableの初期化変更 buildマストに

--- Datastructure/sparse_table.hpp
+++ Datastructure/sparse_table.hpp
@@ -1,38 +1,54 @@
 template <class S, S (*op)(S, S), S (*e)()> struct sparse_table {
     vector<vector<S>> d;
     vector<int> lookup;
-    bool built = true;
+    bool built = false;
+    int n, lg;
+    vector<S> v;
 
-    sparse_table() {}
+    sparse_table(int n) : n(n), v(n, e()) {}
 
-    sparse_table(const vector<S> &v) {
-        int n = v.size();
-        int lg = 0;
+    sparse_table(const vector<S> &v) : n(v.size()), v(v) {}
+
+    void set(int i, S x) {
+        assert(0 <= i && i < n);
+        assert(built == false);
+        v[i] = x;
+    }
+
+    void build() {
+        n = v.size();
+        lg = 0;
         while ((1LL << lg) <= n) lg++;
         d.resize(lg, vector<S>(1LL << lg, e()));
         for (int i = 0; i < n; i++) {
             d[0][i] = v[i];
         }
-
         for (int i = 1; i < lg; i++) {
             for (int j = 0; j + (1LL << i) <= (1LL << lg); j++) {
                 d[i][j] = op(d[i - 1][j], d[i - 1][j + (1LL << (i - 1))]);
             }
         }
-
         lookup.resize(n + 1);
         for (int i = 2; i < int(lookup.size()); i++) {
             lookup[i] = lookup[i >> 1] + 1;
         }
+        built = true;
+    }
+
+    S get(int i) {
+        assert(0 <= i && i < n);
+        return v[i];
     }
 
     S prod(int l, int r) {
-        assert(l <= r);
-        if (l == r) return e();
-        int lg = lookup[r - l];
-        return op(d[lg][l], d[lg][r - (1LL << lg)]);
+        assert(built == true);
+        assert(0 <= l && r <= n);
+        if (l >= r) return e();
+        int LG = lookup[r - l];
+        return op(d[LG][l], d[LG][r - (1LL << LG)]);
     }
 };
+
 /*
-@brief sprase_table
+@brief sparse_table
 */
--- verify/sprase_table.test.cpp
+++ verify/sprase_table.test.cpp
@@ -13,9 +13,13 @@ int main() {
     vec<S> A(n);
     rep(i, 0, n) cin >> A[i];
     stable st(A);
+    stable st2(n);
+    rep(i, 0, n) st2.set(i, A[i]);
+    st.build(); st2.build();
     while(q--) {
         int l, r;
         cin >> l >> r;
+        assert(st.prod(l, r) == st2.prod(l, r));
         cout << st.prod(l, r) << '\n';
     }
 }
