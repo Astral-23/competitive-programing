463bcaffe466afd7be8fd42fafbb6477405b8afd
Astral-23 <ul7cr38@gmail.com>
2024-09-18 23:27:00 +0900

    lzseg 非再起に

--- Datastructure/lazysegtree.hpp
+++ Datastructure/lazysegtree.hpp
@@ -1,4 +1,10 @@
-template<class S, S (*op)(S, S), S (*e)(), class F, S (*mp)(F, S), F (*cm)(F, F), F (*id)()>
+template <class S,
+          S (*op)(S, S),
+          S (*e)(),
+          class F,
+          S (*mp)(F, S),
+          F (*cm)(F, F),
+          F (*id)()>
 struct lazysegtree {
     int n;
     int sz;
@@ -8,21 +14,19 @@ struct lazysegtree {
     lazysegtree(int n) : lazysegtree(vec<S>(n, e())) {}
     lazysegtree(const vec<S> &v) : n((int)(v.size())) {
         log = 1;
-        while((1 << log) < n) log++;
+        while ((1 << log) < n) log++;
         sz = 1 << log;
-        d.resize(2*sz, e());
-        lz.resize(2*sz, id());
-        rep(i, 0, n) d[sz+i] = v[i];
+        d.resize(2 * sz, e());
+        lz.resize(2 * sz, id());
+        rep(i, 0, n) d[sz + i] = v[i];
         rrep(i, 1, sz) update(i);
     }
 
-    void update(int i) {
-        d[i] = op(d[i<<1], d[i<<1|1]);
-    }
+    void update(int i) { d[i] = op(d[i << 1], d[i << 1 | 1]); }
 
     void all_apply(int i, F f) {
         d[i] = mp(f, d[i]);
-        if(i < sz) lz[i] = cm(f, lz[i]);
+        if (i < sz) lz[i] = cm(f, lz[i]);
     }
 
     void push(int k) {
@@ -31,29 +35,60 @@ struct lazysegtree {
         lz[k] = id();
     }
 
-    S prod(int l, int r, int k = 1, int L = 0, int R = -1) {
-        if(R == -1) R = sz;
-        if(r <= L || R <= l) return e();
-        if(l <= L && R <= r) return d[k];
-        push(k);
-        int M = (L + R) / 2;
-        return op(prod(l, r, k * 2, L, M), prod(l, r, k * 2 + 1, M, R));
+    S prod(int l, int r) {
+        assert(0 <= l && l <= r && r <= n);
+        if (l == r) return e();
+
+        l += sz;
+        r += sz;
+
+        for (int i = log; i >= 1; i--) {
+            if (((l >> i) << i) != l) push(l >> i);
+            if (((r >> i) << i) != r) push((r - 1) >> i);
+        }
+
+        S sml = e(), smr = e();
+        while (l < r) {
+            if (l & 1) sml = op(sml, d[l++]);
+            if (r & 1) smr = op(d[--r], smr);
+            l >>= 1;
+            r >>= 1;
+        }
+
+        return op(sml, smr);
     }
 
-    void apply(int l, int r, F f, int k = 1, int L = 0, int R = -1) {
-        if(R == -1) R = sz;
-        if(r <= L || R <= l) return;
-        if(l <= L && R <= r) {
-            all_apply(k, f);
-            return;
+    void apply(int l, int r, F f) {
+        assert(0 <= l && l <= r && r <= n);
+        if (l == r) return;
+
+        l += sz;
+        r += sz;
+
+        for (int i = log; i >= 1; i--) {
+            if (((l >> i) << i) != l) push(l >> i);
+            if (((r >> i) << i) != r) push((r - 1) >> i);
         }
-        push(k);
-        int M = (L + R) / 2;
-        apply(l, r, f, k * 2, L, M); apply(l, r, f, k * 2 + 1, M, R);
-        update(k);
+
+        {
+            int l2 = l, r2 = r;
+            while (l < r) {
+                if (l & 1) all_apply(l++, f);
+                if (r & 1) all_apply(--r, f);
+                l >>= 1;
+                r >>= 1;
+            }
+            l = l2;
+            r = r2;
+        }
+
+        for (int i = 1; i <= log; i++) {
+            if (((l >> i) << i) != l) update(l >> i);
+            if (((r >> i) << i) != r) update((r - 1) >> i);
+        }
     }
-    
-    //以下、必要になったら書く（全て独立)
+
+    // 以下、必要になったら書く（全て独立)
 
     template <class G> int max_right(int l, G g) {
         assert(0 <= l && l <= n);
@@ -81,7 +116,6 @@ struct lazysegtree {
         return n;
     }
 
-   
     template <class G> int min_left(int r, G g) {
         assert(0 <= r && r <= n);
         assert(g(e()));
@@ -108,13 +142,12 @@ struct lazysegtree {
         return 0;
     }
 
-
     void set(int p, S x) {
         assert(0 <= p && p < n);
         p += sz;
-        rrep(i, 1, log+1) push(p >> i);
+        rrep(i, 1, log + 1) push(p >> i);
         d[p] = x;
-        rep(i, 1, log+1) update(p >> i);
+        rep(i, 1, log + 1) update(p >> i);
     }
 
     S all_prod() { return d[1]; }
@@ -133,10 +166,8 @@ struct lazysegtree {
         d[p] = mapping(f, d[p]);
         for (int i = 1; i <= log; i++) update(p >> i);
     }
-
-    
 };
-    
+
 /*
 @brief lazysegtree
 @docs doc/lazysegtree.md
