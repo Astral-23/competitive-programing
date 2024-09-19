c335545ac842c7dc1662a4233c439f8dc8d28e3b
Astral-23 <ul7cr38@gmail.com>
2024-09-20 01:19:20 +0900

    hld 重み付き辺初期化

--- Algorithm/hld.hpp
+++ Algorithm/hld.hpp
@@ -1,12 +1,24 @@
 struct HLD {
     using vi = vec<int>;
     using pi = pair<int, int>;
+    using pll = pair<long long, long long>;
     vi in, out, par, root, rev, dep, pre_vs;
     //          親/成分のtop/inの中身→頂点番号
-    int n, r;//頂点数、根
-    
-  
-    HLD(vec<vi> &g, int a): n(g.size()), r(a) {
+    int n, r;  // 頂点数、根
+
+    static vec<vec<int>> extract_graph(const vec<vec<pll>> &G) {
+        vec<vec<int>> g(G.size());
+        for (int i = 0; i < int(G.size()); i++) {
+            for (auto [w, to] : G[i]) if(i < to) {
+                g[i].push_back(to);
+                g[to].push_back(i);
+            }
+        }
+        return g;
+    }
+    HLD(const vec<vec<pll>> &g, int a) : HLD(extract_graph(g), a) {}
+
+    HLD(vec<vi> g, int a) : n(g.size()), r(a) {
         vi siz(n, 0);
         in = out = root = rev = vi(n);
         par = vi(n, -1);
@@ -15,44 +27,44 @@ struct HLD {
 
         auto dfs_siz = [&](auto f, int v) -> void {
             siz[v]++;
-            for(int& to : g[v]) if(to != par[v]) {
-                dep[to] = dep[v] + 1;
-                par[to] = v;
-                f(f, to);
-                siz[v] += siz[to];
-                if(siz[to] > siz[g[v][0]] || g[v][0] == par[v]) swap(to, g[v][0]);
-            }
+            for (int &to : g[v])
+                if (to != par[v]) {
+                    dep[to] = dep[v] + 1;
+                    par[to] = v;
+                    f(f, to);
+                    siz[v] += siz[to];
+                    if (siz[to] > siz[g[v][0]] || g[v][0] == par[v])
+                        swap(to, g[v][0]);
+                }
             return;
         };
 
         dfs_siz(dfs_siz, r);
-        
+
         int t = 0;
 
         auto dfs_hld = [&](auto f, int v) -> void {
             rev[t] = v;
-            in[v]=t++;
-            for(int to : g[v]) if(to != par[v]) {
-                root[to] = (to == g[v][0] ? root[v] : to);
-                f(f, to);
-            }
+            in[v] = t++;
+            for (int to : g[v])
+                if (to != par[v]) {
+                    root[to] = (to == g[v][0] ? root[v] : to);
+                    f(f, to);
+                }
             out[v] = t;
         };
 
         dfs_hld(dfs_hld, r);
     }
 
+    // 以下、欲しいもののみ書く
 
-    //以下、欲しいもののみ書く
-   
-    int operator()(int v) const {
-        return in[v];
-    }
+    int operator()(int v) const { return in[v]; }
 
     int lca(int a, int b) {
-        while(1) {
-            if(in[a] > in[b]) swap(a, b);
-            if(root[a] == root[b]) return a;
+        while (1) {
+            if (in[a] > in[b]) swap(a, b);
+            if (root[a] == root[b]) return a;
             b = par[root[b]];
         }
     }
@@ -64,36 +76,34 @@ struct HLD {
 
     vec<pi> path(int s, int t, bool edge) {
         vec<pi> ls, rs;
-        while(root[s] != root[t]) {
-            if(dep[root[s]] > dep[root[t]]) {
-                ls.emplace_back(in[s] + 1, in[root[s]]);//上り
+        while (root[s] != root[t]) {
+            if (dep[root[s]] > dep[root[t]]) {
+                ls.emplace_back(in[s] + 1, in[root[s]]);  // 上り
                 s = par[root[s]];
-            }
-            else {
-                rs.emplace_back(in[root[t]], in[t] + 1);//下り
+            } else {
+                rs.emplace_back(in[root[t]], in[t] + 1);  // 下り
                 t = par[root[t]];
             }
         }
 
-        if(dep[s] > dep[t]) ls.emplace_back(in[s] + 1, in[t] + edge);//上り
-        else rs.emplace_back(in[s] + edge, in[t] + 1);//下り
-        
+        if (dep[s] > dep[t])
+            ls.emplace_back(in[s] + 1, in[t] + edge);  // 上り
+        else
+            rs.emplace_back(in[s] + edge, in[t] + 1);  // 下り
+
         reverse(all(rs));
-        for(auto &p : rs) ls.push_back(p);
+        for (auto &p : rs) ls.push_back(p);
         return ls;
     }
 
-    pi subtree(int u, bool edge) {
-        return pi(in[u]+edge, out[u]);
-    }
+    pi subtree(int u, bool edge) { return pi(in[u] + edge, out[u]); }
 
     int kth_ancestor(int v, int k) {
-        if(k > dep[v]) return -1;
-        while(v >= 0) {
-            if(k <= dep[v] - dep[root[v]]) {
+        if (k > dep[v]) return -1;
+        while (v >= 0) {
+            if (k <= dep[v] - dep[root[v]]) {
                 return rev[in[v] - k];
-            }
-            else {
+            } else {
                 k -= dep[v] - dep[root[v]] + 1;
                 v = par[root[v]];
             }
@@ -104,32 +114,34 @@ struct HLD {
         int m = lca(s, t);
         int le = dep[s] - dep[m];
         int ri = dep[t] - dep[m];
-        if(0 <= k && k <= le + ri) {
-            if(k < le) return kth_ancestor(s, k);
-            else return kth_ancestor(t, le + ri - k);
+        if (0 <= k && k <= le + ri) {
+            if (k < le)
+                return kth_ancestor(s, k);
+            else
+                return kth_ancestor(t, le + ri - k);
         }
         return -1;
     }
 
     int aux_tree(vi vs, vec<vi> &g) {
-        if(vs.empty()) return -1;
-        
+        if (vs.empty()) return -1;
+
         auto cmp = [&](int i, int j) { return in[i] < in[j]; };
         sort(all(vs), cmp);
         int m = vs.size();
 
-        rep(i, 0, m-1) vs.push_back(lca(vs[i], vs[i + 1]));
+        rep(i, 0, m - 1) vs.push_back(lca(vs[i], vs[i + 1]));
         sort(all(vs), cmp);
         vs.erase(unique(all(vs)), vs.end());
 
         vi st;
-        for(auto v : vs) {
-            while(st.size()) {
+        for (auto v : vs) {
+            while (st.size()) {
                 int p = st.back();
-                if(in[p] < in[v] && in[v] < out[p]) break;
+                if (in[p] < in[v] && in[v] < out[p]) break;
                 st.pop_back();
             }
-            if(st.size()) {
+            if (st.size()) {
                 g[st.back()].push_back(v);
                 g[v].push_back(st.back());
             }
@@ -141,14 +153,11 @@ struct HLD {
     }
 
     void clean(vec<vi> &g) {
-        for(auto v : pre_vs) g[v] = vi();
+        for (auto v : pre_vs) g[v] = vi();
         pre_vs = vi();
         return;
     }
-};  
-
-
-
+};
 /*
 @brief HLD
 @docs doc/hld.md
