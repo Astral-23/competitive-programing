94a35c0329405d42a70cb51a87479f9f8b76ba4a
Astral-23 <ul7cr38@gmail.com>
2025-01-18 00:12:50 +0900

    a

---
+++ Datastructure/rangeset.hpp
@@ -0,0 +1,77 @@
+template <typename T, bool merge_adju> struct rangeset : public std::map<T, T> {
+    rangeset() {}
+    auto get(T p) const {
+        auto it = (*this).upper_bound(p);
+        if (it == (*this).begin() || (--it)->second <= p) return (*this).end();
+        return it;
+    }
+
+    //[l, r)
+    void insert(T l, T r) {
+        if (l == r) return;
+        assert(l <= r);
+        auto itl = (*this).upper_bound(l),
+             itr = (*this).lower_bound(r + merge_adju);
+        if (itl != (*this).begin() && (--itl)->second + merge_adju <= l) {
+            ++itl;
+        }
+
+        if (itl != itr) {
+            if (itl->first < l) l = itl->first;
+            if (prev(itr)->second > r) r = prev(itr)->second;
+            map<T, T>::erase(itl, itr);
+        }
+        (*this)[l] = r;
+    }
+
+    //[l, r)
+    void erase(T l, T r) {
+        if (l == r) return;
+        assert(l <= r);
+        auto itl = (*this).upper_bound(l), itr = (*this).lower_bound(r);
+        if (itl != (*this).begin() && (--itl)->second <= l) {
+            ++itl;
+        }
+        if (itl == itr) return;
+        T tl = l, tr = r;
+        if (itl->first < l) tl = itl->first;
+        if (prev(itr)->second > r) tr = prev(itr)->second;
+        map<T, T>::erase(itl, itr);
+        if (tl < l) (*this)[tl] = l;
+        if (tr > r) (*this)[r] = tr;
+    }
+
+    bool contains(T p) const { return get(p) != (*this).end(); }
+
+    //[l, r)のうち1要素でも含まれているか
+    bool contains(T l, T r) const {
+        auto itl = (*this).upper_bound(l), itr = (*this).lower_bound(r);
+        if(itl != (*this).begin() && (--itl) -> second <= l) {
+            ++itl;
+        }
+        if(itl == itr) return false;
+        else return true;
+    }
+
+    bool same(T a, T b) const {
+        if (a > b) swap(a, b);
+
+        auto it = get(a);
+        if (it == (*this).end()) return false;
+        return b < it->second;
+    }
+
+    T mex(T x = 0) const {
+        auto it = get(x);
+        if (it == (*this).end())
+            return x;
+        else
+            return it->second;
+    }
+
+    template <typename TYPE, bool ME>
+    friend ostream &operator<<(ostream &os, rangeset<TYPE, ME> const &rhs) {
+        for (auto [l, r] : rhs) os << "[" << l << ", " << r << ")";
+        return os;
+    }
+};
---
+++ verify/Datastructure_rangeset.test.cpp
@@ -0,0 +1,45 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/beta/room.html#RitsCamp18Day1/problems/G"
+#include "../Utility/template.hpp"
+#include "../Datastructure/rangeset.hpp"
+
+int main() {
+    ll n, m, q;
+    cin >> n >> m >> q;
+    using P = array<ll, 4>;
+
+    rangeset<ll, false> rs;
+    vec<P> es;
+    rep(i, 0, m) {
+        ll d, a, b;
+        cin >> d >> a >> b;
+        a--;
+        es.push_back(P{d, 1, a, b});
+    }
+
+    rep(i, 0, q) {
+        ll e, s, t;
+        cin >> e >> s >> t;
+        s--;
+        es.push_back(P{e, -i, s, t});
+    }
+
+    sort(all(es));
+    vec<ll> ans(q);
+    for (auto [d, type, a, b] : es) {
+        if (type != 1) {
+            ll id = -type;
+            if(a >= b - 1) ans[id] = true;
+            else ans[id] = rs.same(a, b - 1);
+        } else {
+            rs.insert(a, b);
+        }
+    }
+
+    rep(i, 0, q) {
+        if (ans[i]) {
+            cout << "Yes" << endl;
+        } else {
+            cout << "No" << endl;
+        }
+    }
+}
