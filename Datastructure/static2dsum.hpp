39c2a7eee30a6b271d5713fccc25dd6284aac28b
Astral-23 <ul7cr38@gmail.com>
2025-02-13 07:41:09 +0900

    a

--- Datastructure/static2dsum.hpp
+++ Datastructure/static2dsum.hpp
@@ -1,59 +1,63 @@
+
 TT struct static2dsum {
+    int id(int i, int j) const { return i * (w + 1) + j; }
     int h, w;
-    vec<vec<T>> dat;
-    bool f = false;
+    vector<T> d;
+    bool built = false;
 
     static2dsum(int h = 0, int w = 0)
-        : static2dsum(vec<vec<T>>(h, vec<T>(w, T()))) {}
+        : static2dsum(vector<vector<T>>(h, vector<T>(w, T()))) {}
 
-    static2dsum(vec<vec<T>> dat) : dat(dat) {
+    static2dsum(vec<vec<T>> const &dat) {
         h = dat.size();
         if (h)
             w = dat[0].size();
         else
             w = 0;
+        d.resize((h + 1) * (w + 1), 0);
+        for (int i = 0; i < h; ++i) {
+            for (int j = 0; j < w; ++j) {
+                d[id(i + 1, j + 1)] = dat[i][j];
+            }
+        }
     }
-
     void add(int i, int j, T x) {
-        assert(f == false);
-        dat[i][j] += x;
+        assert(built == false);
+        d[id(i + 1, j + 1)] += x;
     }
 
     void build() {
-        assert(f == false);
-        rep(i, 0, h) {
-            rep(j, 0, w - 1) { dat[i][j + 1] += dat[i][j]; }
+        assert(built == false);
+        for (int i = 0; i <= h; ++i) {
+            for (int j = 0; j < w; ++j) {
+                d[id(i, j + 1)] += d[id(i, j)];
+            }
         }
 
-        rep(j, 0, w) {
-            rep(i, 0, h - 1) { dat[i + 1][j] += dat[i][j]; }
+        for (int j = 0; j <= w; ++j) {
+            for (int i = 0; i < h; ++i) {
+                d[id(i + 1, j)] += d[id(i, j)];
+            }
         }
 
-        f = true;
+        built = true;
     }
 
     T get(int y, int x) const {
+        assert(built);
         assert(0 <= y && y < h);
         assert(0 <= x && x < w);
         return prod(y, y + 1, x, x + 1);
     }
-    
-    T prod(int sy, int ty, int sx, int tx) const {
-        assert(f);
-        assert(0 <= sy && ty <= h);
-        assert(0 <= sx && tx <= w);
-        assert(sy <= ty);
-        assert(sx <= tx);
-        if(sy == ty || sx == tx) return 0;
-        tx--, ty--;
-        T res = dat[ty][tx];
-        if (sx > 0) res -= dat[ty][sx - 1];
-        if (sy > 0) res -= dat[sy - 1][tx];
-        if (sx > 0 && sy > 0) res += dat[sy - 1][sx - 1];
+
+    T prod(int sx, int tx, int sy, int ty) const {
+        assert(built);
+        assert(0 <= sx && sx <= tx && tx <= h);
+        assert(0 <= sy && sy <= ty && ty <= w);
+        T res = d[id(tx, ty)];
+        res -= d[id(tx, sy)];
+        res -= d[id(sx, ty)];
+        res += d[id(sx, sy)];
         return res;
     }
 };
-/*
-@brief 2次元累積和
-@docs doc/static2dsum.md
-*/
