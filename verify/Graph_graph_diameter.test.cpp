6611720537039f900ed1cdf8d246e0284e6bc221
Astral-23 <ul7cr38@gmail.com>
2025-04-12 22:30:58 +0900

    色々

---
+++ verify/Graph_graph_diameter.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/tree_diameter"
+#include "../Utility/template.hpp"
+#include "../Graph/graph.hpp"
+
+
+int main() {
+    int n;
+    cin >> n;
+    Tree<ll> cyc(n);
+    rep(i, 0, n-1) {
+        ll u, v, c;
+        cin >> u >> v >> c;
+        cyc.add(u, v, c);
+    }
+
+    auto [len, uv] = Tree_lib::diam(cyc);
+    auto [u, v] = uv;
+    auto ps = Tree_lib::path(cyc, u, v);
+    cout << u << " " << v << endl;
+    for(auto vv : ps) cout << vv << " ";
+}
